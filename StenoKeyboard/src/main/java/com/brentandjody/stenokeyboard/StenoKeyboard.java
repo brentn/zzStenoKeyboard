872f3d5f69ecb915f175f586af2b3d845a99f4fe
Brent Nesbitt <nesbitt.brent@gmail.com>
2013-10-23 13:57:46 -0700

    NoTests: Added basic UI

--- StenoKeyboard/src/main/java/com/brentandjody/stenokeyboard/StenoKeyboard.java
+++ StenoKeyboard/src/main/java/com/brentandjody/stenokeyboard/StenoKeyboard.java
@@ -1,9 +1,74 @@
 package com.brentandjody.stenokeyboard;
 
 import android.inputmethodservice.InputMethodService;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 /**
  * Created by brent on 16/10/13.
  */
 public class StenoKeyboard extends InputMethodService {
+
+    private Button sendButton;
+    private Dictionary dictionary;
+    private TouchLayer keyboardView;
+    private LinearLayout candidatesView;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        dictionary = new Dictionary(getApplicationContext());
+    }
+
+
+//    @Override
+//    public boolean onEvaluateFullscreenMode() {
+//        return true;
+//    }
+
+
+    @Override
+    public View onCreateInputView() {
+        super.onCreateInputView();
+        LayoutInflater layoutInflater = getLayoutInflater();
+        keyboardView = (TouchLayer) layoutInflater.inflate(R.layout.keyboard, null);
+        sendButton = (Button) keyboardView.findViewById(R.id.send_button);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String stroke = keyboardView.getStroke();
+                String message = dictionary.translate(stroke);
+                populateCandidates(dictionary.getCandidateStrings());
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+                getCurrentInputConnection().commitText(message, 1);
+            }
+        });
+        return keyboardView;
+    }
+
+    @Override
+    public View onCreateCandidatesView() {
+        super.onCreateCandidatesView();
+        LayoutInflater layoutInflater = getLayoutInflater();
+        candidatesView = (LinearLayout) layoutInflater.inflate(R.layout.candidates, null);
+        return candidatesView;
+    }
+
+    private void populateCandidates(List<String> items) {
+        candidatesView.removeAllViews();
+        LayoutInflater layoutInflater = getLayoutInflater();
+        for (String item : items) {
+            TextView tv = (TextView) layoutInflater.inflate(R.layout.candidate, null);
+            tv.setText(item);
+            candidatesView.addView(tv);
+        }
+        setCandidatesViewShown(candidatesView.getChildCount() > 0);
+    }
+
 }
---
+++ StenoKeyboard/src/main/java/com/brentandjody/stenokeyboard/TouchLayer.java
@@ -0,0 +1,148 @@
+package com.brentandjody.stenokeyboard;
+
+import android.content.Context;
+import android.graphics.Point;
+import android.util.AttributeSet;
+import android.view.Display;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by brent on 20/10/13.
+ */
+public class TouchLayer extends LinearLayout {
+
+    private static final int TOUCH_RADIUS = 25;
+    private List<Button> keys = new ArrayList<Button>();
+    private Button fKey;
+
+
+    public TouchLayer(Context context) {
+        super(context);
+    }
+
+    public TouchLayer(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public TouchLayer(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+        keys.clear();
+        enumerateKeys(this);
+        // all this is to set the height of the keyboard
+        WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int screen_height = size.y;
+        this.getLayoutParams().height = screen_height / 3;
+    }
+
+    private void enumerateKeys(View v) {
+        //list steno keys IN ORDER
+        fKey = (Button) v.findViewById(R.id._F);
+        keys.add((Button) v.findViewById(R.id.number_bar));
+        keys.add((Button) v.findViewById(R.id.S));
+        keys.add((Button) v.findViewById(R.id.T));
+        keys.add((Button) v.findViewById(R.id.K));
+        keys.add((Button) v.findViewById(R.id.P));
+        keys.add((Button) v.findViewById(R.id.W));
+        keys.add((Button) v.findViewById(R.id.H));
+        keys.add((Button) v.findViewById(R.id.R));
+        keys.add((Button) v.findViewById(R.id.A));
+        keys.add((Button) v.findViewById(R.id.O));
+        keys.add((Button) v.findViewById(R.id.star));
+        keys.add((Button) v.findViewById(R.id._E));
+        keys.add((Button) v.findViewById(R.id._U));
+        keys.add((Button) v.findViewById(R.id._F));
+        keys.add((Button) v.findViewById(R.id._R));
+        keys.add((Button) v.findViewById(R.id._P));
+        keys.add((Button) v.findViewById(R.id._B));
+        keys.add((Button) v.findViewById(R.id._L));
+        keys.add((Button) v.findViewById(R.id._G));
+        keys.add((Button) v.findViewById(R.id._T));
+        keys.add((Button) v.findViewById(R.id._S));
+        keys.add((Button) v.findViewById(R.id._D));
+        keys.add((Button) v.findViewById(R.id._Z));
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // iterate over all keys, and toggle all those within TOUCH_RADIUS
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            checkKeyPressed(event, event.getX(), event.getY());
+        }
+//        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+//            case MotionEvent.ACTION_DOWN:
+//                checkKeyPressed(event, event.getX(), event.getY());
+//                break;
+//            case MotionEvent.ACTION_POINTER_DOWN:
+//                for (int i=0; i < event.getPointerCount(); i++) {
+//                    checkKeyPressed(event, event.getX(i), event.getY(i));
+//                }
+//        }
+        return true;
+    }
+
+    private void checkKeyPressed(MotionEvent event, float X, float Y) {
+        float top, left, bottom, right;
+        int[] location = new int[2];
+        this.getLocationOnScreen(location);
+        int Xoffset = location[0];
+        int Yoffset = location[1];
+        float minX = X + Xoffset - TOUCH_RADIUS;
+        float maxX = X + Xoffset + TOUCH_RADIUS;
+        float minY = Y + Yoffset - TOUCH_RADIUS;
+        float maxY = Y + Yoffset + TOUCH_RADIUS;
+        for (Button key:keys) {
+
+            key.getLocationOnScreen(location);
+            left = location[0];
+            top = location[1];
+            right = left+key.getWidth();
+            bottom = top+key.getHeight();
+            if ((maxX > left) && (minX < right)) {
+                if ((maxY > top) && (minY < bottom)) {
+                    key.setSelected(! key.isSelected());
+                }
+            }
+        }
+    }
+
+    public String getStroke() {
+        String result = "";
+        Boolean hasVowels = false;
+        for (Button key : keys) {
+            if (key == fKey && (! hasVowels)) {
+                result += "-";
+            }
+            if (key.isSelected()) {
+                if ("AOEU".contains(key.getText())) hasVowels = true;
+                result += key.getText();
+                key.setSelected(false);
+            }
+        }
+        return result;
+    }
+
+
+}
